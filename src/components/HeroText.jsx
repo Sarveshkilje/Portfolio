d00ea91c9709496a70a0e06fcfd50dfd412cdb77
Sarvesh Kilje <sarvesh395kilje@gmail.com>
2025-10-11 17:15:33 +0530

    all copied

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -2,7 +2,7 @@ import { FlipWords } from "./FlipWords";
 import { motion } from "motion/react";
 
 const HeroText = () => {
-  const words = ["Secure", "Modern", "Scalable"];
+  const words = ["Web Developer", "Ui/Ux Desginer"];
   const variants = {
     hidden: { opacity: 0, x: -50 },
     visible: { opacity: 1, x: 0 },
@@ -10,9 +10,9 @@ const HeroText = () => {
   return (
     <div className="z-10 mt-20 text-center md:mt-40 md:text-left rounded-3xl bg-clip-text">
       {/* Desktop View */}
-      <div className="flex-col hidden md:flex c-space">
+      <div className="flex-col hidden lg:flex c-space">
         <motion.h1
-          className="text-4xl font-medium"
+          className="text-5xl font-medium"
           variants={variants}
           initial="hidden"
           animate="visible"
@@ -22,38 +22,38 @@ const HeroText = () => {
         </motion.h1>
         <div className="flex flex-col items-start">
           <motion.p
-            className="text-5xl font-medium text-neutral-300"
+            className="text-3xl font-medium text-neutral-300"
             variants={variants}
             initial="hidden"
             animate="visible"
             transition={{ delay: 1.2 }}
           >
-          
+            A Passionate
           </motion.p>
+                              <FlipWords
+              words={words}
+              className="font-black text-white text-7xl"
+            />
           <motion.div
             variants={variants}
             initial="hidden"
             animate="visible"
             transition={{ delay: 1.5 }}
           >
-            <FlipWords
-              words={words}
-              className="font-black text-white text-8xl"
-            />
           </motion.div>
           <motion.p
-            className="text-4xl font-medium text-neutral-300"
+            className="text-2xl font-medium text-neutral-300"
             variants={variants}
             initial="hidden"
             animate="visible"
             transition={{ delay: 1.8 }}
           >
-            Web Solutions
+            turning ideas into interactive realities.
           </motion.p>
         </div>
       </div>
       {/* Mobile View */}
-      <div className="flex- flex-col space-y-6 md:hidden">
+      <div className="flex- flex-col mt-20 lg:hidden">
         <motion.p
           className="text-4xl font-medium"
           variants={variants}
@@ -61,37 +61,37 @@ const HeroText = () => {
           animate="visible"
           transition={{ delay: 1 }}
         >
-          Hi,I'm Ali
+          Hi,I'm Sarvesh
         </motion.p>
         <div>
           <motion.p
-            className="text-5xl font-black text-neutral-300"
+            className="text-1xl font-black text-neutral-300"
             variants={variants}
             initial="hidden"
             animate="visible"
             transition={{ delay: 1.2 }}
           >
-            Building
+            A Passionate
           </motion.p>
+                               <FlipWords
+              words={words}
+              className="font-bold text-white text-4xl"
+            />
           <motion.div
             variants={variants}
             initial="hidden"
             animate="visible"
             transition={{ delay: 1.5 }}
           >
-            <FlipWords
-              words={words}
-              className="font-bold text-white text-7xl"
-            />
           </motion.div>
           <motion.p
-            className="text-4xl font-black text-neutral300"
+            className="text-1xl font-black text-neutral300"
             variants={variants}
             initial="hidden"
             animate="visible"
             transition={{ delay: 1.8 }}
           >
-            Web Applications
+            create interactive UI.
           </motion.p>
         </div>
       </div>
--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -17,7 +17,7 @@ const About = () => {
             className="absolute scale-[1.75] -right-[5rem] -top-[1rem] md:scale-[3] md:left-50 md:inset-y-10 lg:scale-[2.5]"
           />
           <div className="z-10">
-            <p className="headtext">Hi, I'm Ali Sanati</p>
+            <p className="headtext">Hi, I'm Sarvesh Kilje</p>
             <p className="subtext">
               Over the last 4 years, I developed my frontend and backend dev
               skills to deliver dynamic and software and web applications.
@@ -31,32 +31,30 @@ const About = () => {
             ref={grid2Container}
             className="flex items-center justify-center w-full h-full"
           >
-            <p className="flex items-end text-5xl text-gray-500">
-              CODE IS CRAFT
-            </p>
+            <p className="flex items-end text-5xl text-gray-500">SERVICES</p>
             <Card
               style={{ rotate: "75deg", top: "30%", left: "20%" }}
-              text="GRASP"
+              text="Web Dev"
               containerRef={grid2Container}
             />
             <Card
               style={{ rotate: "-30deg", top: "60%", left: "45%" }}
-              text="SOLID"
+              text="UI/UX"
               containerRef={grid2Container}
             />
             <Card
               style={{ rotate: "90deg", bottom: "30%", left: "70%" }}
-              text="Design Patterns"
+              text="Video Editing"
               containerRef={grid2Container}
             />
             <Card
               style={{ rotate: "-45deg", top: "55%", left: "0%" }}
-              text="Design Principles"
+              text=" Graphic Designing"
               containerRef={grid2Container}
             />
             <Card
               style={{ rotate: "20deg", top: "10%", left: "38%" }}
-              text="SRP"
+              text="ReatJS Developer"
               containerRef={grid2Container}
             />
             <Card
@@ -79,12 +77,13 @@ const About = () => {
         {/* Grid 3 */}
         <div className="grid-black-color grid-3">
           <div className="z-10 w-[50%]">
-            <p className="headtext">Time Zone</p>
+            <p className="headtext">Learning Journey</p>
             <p className="subtext">
-              I'm based in Mars, and open to remote work worldwide
+              Currently pursuing a B.Tech in Computer Engineering.<br></br>
+              Focused on mastering clean code, React, and backend development
             </p>
           </div>
-          <figure className="absolute left-[30%] top-[10%]">
+          <figure className="absolute left-[40%] top-[10%]">
             <Globe />
           </figure>
         </div>
@@ -103,7 +102,11 @@ const About = () => {
             <p className="headText">Teck Stack</p>
             <p className="subtext">
               I specialize in a variety of languages, frameworks, and tools taht
-              allow me to build robust and scalable applications
+              allow me to build robust and scalable applications.
+              <br></br>
+              <span className="py-5 font-medium">
+                <b>Scroll Down</b> to know more about my skills.
+              </span>
             </p>
           </div>
           <div className="absolute inset-y-0 md:inset-y-9 w-full h-full start-[50%] md:scale-125">
